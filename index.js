b2235687b137ef523cea358a3a3687c72e3e7b34
_BLacK_JacK_ <iiggy2661@gmail.com>
2023-01-28 12:05:31 +0200

    test

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const authRouter = require('./authRouter')
 
 const app = express();
 const port = 5000;
 
-app.use('/', (req, res) => {
-    res.json({message: "Hello!"})
-})
+app.use(express.json())
+
+app.use("/auth", authRouter)
 
 app.listen(port, () => {
     console.log(`Starting on port ${port}`);
